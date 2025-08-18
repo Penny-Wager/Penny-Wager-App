fa632c101ddc1338f5a41ea5f7e7169a01fecb8e
vilio316 <atembarry12@gmail.com>
2025-08-18 14:03:14 +0100

    Atomic: Multiplier, bet states and components added

--- src/game/CardPickGame.jsx
+++ src/game/CardPickGame.jsx
@@ -7,7 +7,9 @@ import { GiDiamonds } from "react-icons/gi";
 
 export default function CardPickGame(){
     const [isShuffling, setShuffleStatus] = useState(false)
-    const [betValue, updateBet] = useState(0)
+    const [betValue, updateBet] = useState(0.1)
+    const [betMultiplier, updateMultiplier] = useState(1.0)
+    const [attempts, addAttempt] = useState(0)
     const [selectedValue, selectNewValue] = useState('')
     const [selectedSuit, selectNewSuit] = useState('')
     const [shouldStartShuffle, setShuff] = useState(false)
@@ -24,6 +26,7 @@ export default function CardPickGame(){
         , 3000) //Simulates loading for the card shuffle
     }
 
+    //function used to generate the "Lucky Card"
     function luckyChoose(){
         const suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
         const values = ["K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A"]
@@ -35,6 +38,8 @@ export default function CardPickGame(){
 
     }
 
+
+    //Shows card icons depending on the card suit
       function CardIcon({suit}){
             switch (suit){
                 case "Hearts":
@@ -49,6 +54,7 @@ export default function CardPickGame(){
         }
 
 
+    //Right-Hand Side Component
     function GameComp(){
         const {suit, value} =  luckyChoose()
 
@@ -69,7 +75,7 @@ export default function CardPickGame(){
                     </div>
                 </div> 
                 </div>   
-                <button className="rounded-xl text-white bg-indigo-700 p-2">Play Again</button>
+                <button className="rounded-xl text-white bg-indigo-700 p-2" onClick={()=> setShuff(false)}>Play Again</button>
             </div>
         )
     }
@@ -89,14 +95,14 @@ export default function CardPickGame(){
                     <option value={"Q"}>Queen</option>
                     <option value={"J"}>Jack</option>
                     <option value={"10"}>10</option>
-                    <option value={"9.0"}>9</option>
-                    <option value={"8.0"}>8</option>
-                    <option value={"7.0"}>7</option>
-                    <option value={"6.0"}>6</option>
-                    <option value={"5.0"}>5</option>
-                    <option value={"4.0"}>4</option>
-                    <option value={"3.0"}>3</option>
-                    <option value={"2.0"}>2</option>
+                    <option value={"9"}>9</option>
+                    <option value={"8"}>8</option>
+                    <option value={"7"}>7</option>
+                    <option value={"6"}>6</option>
+                    <option value={"5"}>5</option>
+                    <option value={"4"}>4</option>
+                    <option value={"3"}>3</option>
+                    <option value={"2"}>2</option>
                     <option value={"A"}>Ace</option>
                 </select>
                 </label>
@@ -116,14 +122,24 @@ export default function CardPickGame(){
                 <label htmlFor="amnt" className="bold">
                     Bet Amount:
                 <div className="flex gap-x-2 border-2 border-zinc-800 rounded-xl">
-                <input type="number" name="betAmount" id="amnt" step={0.01} min={0.1} max={5.0} className="block indent-3  w-[75%] outline-0" defaultValue={0.1} onChange={(e) => updateBet(e.target.value)}/>
+                <input type="number" name="betAmount" id="amnt" step={0.01} min={0.10} max={5.0} className="block indent-3  w-[75%] outline-0" defaultValue={0.1} onChange={(e) => updateBet(e.target.value)}/>
                 <span className="border-l-2 border-zinc-800 p-1 md:p-2">MON</span>
                 </div>
                 </label> 
 
+                <label>Multiplier: </label>
+                <div className="grid grid-cols-4 gap-x-2">
+
+                <button onClick={()=>updateMultiplier(1.25)} className="rounded-[1.25rem] bg-green-400 text-green-200 hover:text-white">x1.25</button>
+                <button onClick={()=>updateMultiplier(2.43)} className="rounded-[1.25rem] bg-amber-500 text-amber-300 hover:text-white">x2.43</button>
+                <button onClick={()=>updateMultiplier(1.34)} className="rounded-[1.25rem] bg-green-400 text-green-200 hover:text-white">x1.34</button>
+                <button onClick={()=>updateMultiplier(4.23)} className="rounded-[1.25rem] bg-red-500 text-red-300 hover:text-white">x4.23</button>
+             
                 </div>
-
-
+                   <p className="my-2">
+                    Pot. Win: {(betValue * betMultiplier).toFixed(4)} MON
+                </p>
+</div>
                 <div className="grid">
                 <div className="card_preview my-2 place-self-center md:my-4 p-2 md:p-4 bg-white h-[7.5rem] w-[5rem] rounded-xl text-black" >
                     <div className="grid relative h-full">
@@ -145,7 +161,7 @@ export default function CardPickGame(){
             <div className="grid place-items-center h-[7.5rem] md:h-auto border-t-2 md:border-t-0 md:border-l-2 border-zinc-800">
                {
                 isShuffling ? <p>Shuffling!</p> : <p>{
-                    shouldStartShuffle && selectedSuit.length >1 && selectedValue.length == 1 ? <GameComp/>: 'Awaiting card choice'
+                    shouldStartShuffle && selectedSuit.length >1 && selectedValue.length >= 1 ? <GameComp/>: 'Awaiting card choice'
                     }</p>
                } 
             </div>
