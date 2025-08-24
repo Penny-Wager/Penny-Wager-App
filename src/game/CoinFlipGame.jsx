5c3c32134a81be2fd29b4c8424056acf6343d7f6
vilio316 <atembarry12@gmail.com>
2025-08-24 23:23:20 +0100

    Minor UI Updates, Fixed Coin Flipping Bug

--- src/game/CoinFlipGame.jsx
+++ src/game/CoinFlipGame.jsx
@@ -35,8 +35,8 @@ function checker(){
 
 function CoinComponent(){
     return(
-        <div className="grid place-items-center">   
-            <div className={`w-25 h-25 animate rounded-full bg-gradient-to-br from-amber-300 to-amber-700 perspective-[300px] transform-3d place-items-center grid shadow-black shadow-2xl border-4 border-amber-800 ${isFlipping ? "animate" : 'animate-none'} ${isFlipping || isGamePlayed? 'block' : 'hidden'}`}>
+        <div className="grid place-items-center my-2">   
+            <div className={`w-25 h-25 animate rounded-full bg-gradient-to-br from-amber-300 to-amber-700 perspective-[300px] transform-3d place-items-center grid shadow-black shadow-2xl border-4 border-amber-800 ${isFlipping ? "animate" : 'hidden'}`}>
                 {
         coinState ? <FaCrown size={40} className="coinShow"/> : <FaAnchor size={40} className="coinShow"/>
       }
@@ -45,6 +45,13 @@ function CoinComponent(){
 
          {isGamePlayed && attempts > 0 ? 
             <div className={`${isGamePlayed? 'block': "hidden"}`}>
+                <div className="grid place-items-center my-1 md:my-2">
+                <div className={`w-25 h-25 rounded-full bg-gradient-to-br from-amber-300 to-amber-700 perspective-[300px] transform-3d place-items-center grid shadow-black shadow-2xl border-4 border-amber-800`}>
+                        {
+        coinState ? <FaCrown size={40} /> : <FaAnchor size={40}/>
+      }
+      </div>
+      </div>
             <p>
                Value:  {coinState ? "Heads" : "Tails"}
             </p>
@@ -52,11 +59,13 @@ function CoinComponent(){
                 Is Guess Correct: {checker()}
             </p>
 
-                <div className="flex gap-x-4">
-            <button>
+            <div className="flex gap-x-2">
+            <button className="rounded-xl outline-none p-1 md:p-2 disabled:bg-zinc-500 bg-green-400 " disabled={checker
+                () !== "Yes"
+            }>
                 Collect Winnings
             </button>
-            <button onClick={()=> setPlayed(false)}>
+            <button onClick={()=> setPlayed(false)}  className="rounded-xl bg-indigo-600 p-1 md:p-2">
                 Play Again
             </button>
             </div>
@@ -71,7 +80,7 @@ function CoinComponent(){
 
 
     return(
-        <div className="grid grid-cols-2 gap-x-2 bg-zinc-700 border-zinc-800 p-1 md:p-2 border-2">
+        <div className="grid md:grid-cols-2 my-1 md:my-2 gap-2 bg-zinc-700 border-zinc-800 p-1 md:p-2 border-2">
             <div className="grid">
                 <p className="font-bold text-xl">Flip a Coin!</p>
                 <p>Attempts: {attempts}/5</p>
@@ -107,11 +116,9 @@ function CoinComponent(){
                 disabled={attempts > 5 || isGamePlayed}
                     onClick={()=> {
                          flipCoin(true)
+                         flip();
                          setTimeout(()=> {
-                            flipCoin(false)
-                         }, 4000)
-                        setTimeout(()=> {
-                            flip();
+                            flipCoin(false);
                             setPlayed(true);
                         },3000)
                        
